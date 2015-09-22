17e84a10caa7e7527ec254b410b3994f9aeda22e
Harsh Modi <hjmodi1@gmail.com>
2015-09-22 15:46:51 +0530

    Show warning if module is not installed on internal storage
    fixes rovo89/XposedInstaller#252

--- app/src/main/java/de/robv/android/xposed/installer/util/ModuleUtil.java
+++ app/src/main/java/de/robv/android/xposed/installer/util/ModuleUtil.java
@@ -242,6 +242,7 @@ public final class ModuleUtil {
 		private String description; // loaded lazyily
 
 		private Drawable.ConstantState iconCache = null;
+		private static final int FLAG_FORWARD_LOCK = 1<<29;
 
 		private InstalledModule(PackageInfo pkg, boolean isFramework) {
 			this.app = pkg.applicationInfo;
@@ -300,6 +301,14 @@ public final class ModuleUtil {
 			Drawable result = app.loadIcon(mPm);
 			iconCache = result.getConstantState();
 			return result;
+		}
+
+		public boolean isInstalledOnExternalStorage() {
+			return (app.flags & ApplicationInfo.FLAG_EXTERNAL_STORAGE) != 0;
+		}
+
+		public boolean isForwardLocked() {
+			return (app.flags & FLAG_FORWARD_LOCK) != 0;
 		}
 
 		@Override
